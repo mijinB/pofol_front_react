ca4339e4c2e331cd81226481c716151ca4f8c5c4
mijinB <bmj13465@gmail.com>
2023-10-05 16:34:51 +0900

    createBrowserRouter 적용

---
+++ src/Router.tsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "./Root";
+import { About } from "./screens/About";
+import { Home } from "./screens/Home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        children: [
+            {
+                path: "home",
+                element: <Home />,
+            },
+            {
+                path: "about",
+                element: <About />,
+            },
+        ],
+    },
+]);
+
+export default router;
