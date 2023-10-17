92caca169eac4cce084022d7c364baf908b90c9c
mijinB <bmj13465@gmail.com>
2023-10-17 14:47:07 +0900

    기본 layout

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,11 +1,26 @@
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 7fr;
+    grid-template-rows: 1fr 18fr;
+    min-height: 100vh;
+    background-color: red;
+`;
+
+const Aside = styled.div`
+    grid-row: span 2;
+    background-color: blue;
+`;
 
 function Root() {
     return (
-        <div>
+        <Wrapper>
+            <Aside></Aside>
             <h1>root</h1>
             <Outlet />
-        </div>
+        </Wrapper>
     );
 }
 
--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -1,5 +1,21 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    height: 100%;
+    background-color: yellow;
+`;
+
+const TestH1 = styled.h1`
+    font-size: 25px;
+    font-weight: 600;
+`;
+
 function About() {
-    return <h1>about</h1>;
+    return (
+        <Wrapper>
+            <TestH1>about</TestH1>
+        </Wrapper>
+    );
 }
 
 export default About;
