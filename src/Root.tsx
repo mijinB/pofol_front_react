083f0fa0729fdab975c8e75ea70db57afc729d9a
mijinB <bmj13465@gmail.com>
2023-10-22 16:32:59 +0900

    option 팝업창) 열려있을 때 다른 영역 누르면 닫히도록 overlay 추가

--- src/Root.tsx
+++ src/Root.tsx
@@ -98,6 +98,14 @@ const OptionsButton = styled.button`
     padding: 6px;
 `;
 
+const OverLay = styled.div`
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+`;
+
 const OptionsPopup = styled.div`
     position: absolute;
     top: 57px;
@@ -323,38 +331,41 @@ function Root() {
                         </svg>
                     </OptionsButton>
                     {isOptionsPopupOpen ? (
-                        <OptionsPopup>
-                            <OptionItem onClick={toggleFullWidth} $isFullWidth={isFullWidth}>
-                                <span>전체 너비</span>
-                                <FullWidthToggle onClick={toggleFullWidth} />
-                                <FullWidthLabel $isFullWidth={isFullWidth} />
-                            </OptionItem>
-                            <OptionItem onClick={() => onCopyClipBoard(`${BASE_URL}${location.pathname}`)}>
-                                <span>링크 복사</span>
-                                <svg
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        d="M14 7V7C14 6.06812 14 5.60218 13.8478 5.23463C13.6448 4.74458 13.2554 4.35523 12.7654 4.15224C12.3978 4 11.9319 4 11 4H8C6.11438 4 5.17157 4 4.58579 4.58579C4 5.17157 4 6.11438 4 8V11C4 11.9319 4 12.3978 4.15224 12.7654C4.35523 13.2554 4.74458 13.6448 5.23463 13.8478C5.60218 14 6.06812 14 7 14V14"
-                                        stroke="#37352F"
-                                        strokeWidth="2"
-                                    />
-                                    <rect
-                                        x="10"
-                                        y="10"
-                                        width="10"
-                                        height="10"
-                                        rx="2"
-                                        stroke="#37352F"
-                                        strokeWidth="2"
-                                    />
-                                </svg>
-                            </OptionItem>
-                        </OptionsPopup>
+                        <>
+                            <OverLay onClick={toggleOptionsPopup} />
+                            <OptionsPopup>
+                                <OptionItem onClick={toggleFullWidth} $isFullWidth={isFullWidth}>
+                                    <span>전체 너비</span>
+                                    <FullWidthToggle onClick={toggleFullWidth} />
+                                    <FullWidthLabel $isFullWidth={isFullWidth} />
+                                </OptionItem>
+                                <OptionItem onClick={() => onCopyClipBoard(`${BASE_URL}${location.pathname}`)}>
+                                    <span>링크 복사</span>
+                                    <svg
+                                        width="24"
+                                        height="24"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            d="M14 7V7C14 6.06812 14 5.60218 13.8478 5.23463C13.6448 4.74458 13.2554 4.35523 12.7654 4.15224C12.3978 4 11.9319 4 11 4H8C6.11438 4 5.17157 4 4.58579 4.58579C4 5.17157 4 6.11438 4 8V11C4 11.9319 4 12.3978 4.15224 12.7654C4.35523 13.2554 4.74458 13.6448 5.23463 13.8478C5.60218 14 6.06812 14 7 14V14"
+                                            stroke="#37352F"
+                                            strokeWidth="2"
+                                        />
+                                        <rect
+                                            x="10"
+                                            y="10"
+                                            width="10"
+                                            height="10"
+                                            rx="2"
+                                            stroke="#37352F"
+                                            strokeWidth="2"
+                                        />
+                                    </svg>
+                                </OptionItem>
+                            </OptionsPopup>
+                        </>
                     ) : null}
                 </PageHeader>
                 <PageContainer $isFullWidth={isFullWidth}>
