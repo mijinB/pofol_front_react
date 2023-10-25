c733d868307c55d59606ab81e3a9f1baf2d73cce
mijinB <bmj13465@gmail.com>
2023-10-25 19:22:31 +0900

    project_modal) 스크롤 기능 추가하면서 레이아웃도 수정 & link 임시 UI

--- src/screens/Project.tsx
+++ src/screens/Project.tsx
@@ -107,7 +107,7 @@ const ProjectDetailsModal = styled.div`
     top: 50%;
     left: 50%;
     display: grid;
-    grid-template-rows: 44px 1fr 2.5fr;
+    grid-template-rows: 44px 1fr;
     width: 50%;
     height: 85%;
     border-top-left-radius: 5px;
@@ -117,12 +117,6 @@ const ProjectDetailsModal = styled.div`
     box-shadow: rgba(15, 15, 15, 0.016) 0px 0px 0px 1px, rgba(15, 15, 15, 0.03) 0px 3px 6px,
         rgba(15, 15, 15, 0.06) 0px 9px 24px;
     transform: translate(-50%, -50%);
-    overflow: hidden;
-    > img {
-        position: absolute;
-        top: 216px;
-        left: 125px;
-    }
 `;
 
 const ModalHeader = styled.div`
@@ -139,14 +133,33 @@ const ModalHeader = styled.div`
     }
 `;
 
-const ModalImageContainer = styled.div`
-    overflow: hidden;
-`;
-const ModalImage = styled.img`
-    width: 100%;
+const ModalContentWrapper = styled.div`
+    display: grid;
+    grid-template-rows: 220px 1fr;
+    overflow-y: scroll;
 `;
 
-const ModalTextContainer = styled.div`
+const ModalImageContainer = styled.div<{ $todoLogo: string }>`
+    position: relative;
+    &::after {
+        position: absolute;
+        bottom: -50px;
+        left: 120px;
+        width: 100px;
+        height: 100px;
+        background: url(${(props) => props.$todoLogo}) no-repeat center/contain;
+        content: "";
+    }
+`;
+
+const ModalImage = styled.img`
+    width: 100%;
+    height: 220px;
+    object-fit: cover;
+    object-position: top;
+`;
+
+const ModalTextContent = styled.div`
     padding: 80px 110px;
 `;
 
@@ -210,6 +223,30 @@ const CommentContent = styled.div`
     }
 `;
 
+const ModalContentSection = styled.div``;
+
+const SectionTitle = styled.h3`
+    font-size: 22px;
+    font-weight: 600;
+`;
+
+const LinkItem = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    margin: 20px 0;
+    > span {
+        font-size: 15px;
+        font-style: italic;
+    }
+`;
+
+const LinkItemBox = styled.a`
+    padding: 10px 15px;
+    border: 1px solid rgba(55, 53, 47, 0.2);
+    border-radius: 4px;
+`;
+
 interface IProjectProp {
     isFullWidth: boolean;
 }
@@ -266,99 +303,107 @@ function Project() {
                                     <path d="M15 15L9 9" stroke="#37352F" />
                                 </svg>
                             </ModalHeader>
-                            <ModalImageContainer>
-                                <ModalImage src={todoTemplateImage} alt="todo template" />
-                            </ModalImageContainer>
-                            <img src={todoLogo} alt="todo logo" width={90} />
-                            <ModalTextContainer>
-                                <ModalTitle>To Do List</ModalTitle>
-                                <ModalSummaryContainer>
-                                    <ModalSummary>
-                                        <SummaryItemTitle>
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                width="21"
-                                                height="21"
-                                                viewBox="0 0 64 64"
-                                            >
-                                                <defs fill="#37352F" />
-                                                <path
-                                                    d="m32,8c-14.81,0-24,8.43-24,22,0,6.92,2.4,12.49,6.7,16.3l-6.7,6.7,2,3c5.42,0,10.43-1.8,14.45-4.84,2.32.55,4.84.84,7.55.84,14.81,0,24-8.43,24-22s-9.19-22-24-22Zm4,37h-8v-6h8v6Zm0-18l-2,8h-4l-2-8v-12h8v12Z"
-                                                    fill="#37352F"
-                                                />
-                                            </svg>
-                                            <span>간단 소개</span>
-                                        </SummaryItemTitle>
-                                        <SummaryItemContent>
-                                            <p>해야 하는 일들의 정리를 도와주는 web application입니다.</p>
-                                        </SummaryItemContent>
-                                        <SummaryItemTitle>
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                width="21"
-                                                height="21"
-                                                viewBox="0 0 24 24"
-                                                fill="none"
-                                            >
-                                                <path
-                                                    d="M12 20L4.6797 10.8496C4.34718 10.434 4.18092 10.2262 4.13625 9.9757C4.09159 9.72524 4.17575 9.47276 4.34407 8.96778L5.0883 6.73509C5.52832 5.41505 5.74832 4.75503 6.2721 4.37752C6.79587 4 7.49159 4 8.88304 4H15.117C16.5084 4 17.2041 4 17.7279 4.37752C18.2517 4.75503 18.4717 5.41505 18.9117 6.73509L19.6559 8.96778C19.8243 9.47276 19.9084 9.72524 19.8637 9.9757C19.8191 10.2262 19.6528 10.434 19.3203 10.8496L12 20ZM12 20L15.5 9M12 20L8.5 9M19.5 10L15.5 9M15.5 9L14 5M15.5 9H8.5M10 5L8.5 9M8.5 9L4.5 10"
-                                                    stroke="#37352F"
-                                                    strokeLinecap="round"
-                                                />
-                                            </svg>
-                                            <span>skills & tool</span>
-                                        </SummaryItemTitle>
-                                        <SummaryItemContent>
-                                            <ToDoSkills />
-                                        </SummaryItemContent>
-                                        <SummaryItemTitle>
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                width="21"
-                                                height="21"
-                                                viewBox="0 0 64 64"
-                                            >
-                                                <defs fill="#37352F" />
-                                                <path
-                                                    d="m48,12v-6h-6v6h-20v-6h-6v6h-8v40h48V12h-8Zm2,35H14v-19h36v19Z"
-                                                    fill="#37352F"
-                                                />
-                                            </svg>
-                                            <span>진행 기간</span>
-                                        </SummaryItemTitle>
-                                        <SummaryItemContent>
-                                            <p>2023년 8월 18일 → 2023년 8월 29일</p>
-                                        </SummaryItemContent>
-                                        <SummaryItemTitle>
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                width="21"
-                                                height="21"
-                                                viewBox="0 0 64 64"
-                                            >
-                                                <defs fill="#37352F" />
-                                                <path
-                                                    d="m27,19c0-3.09,1.91-5,5-5s5,1.91,5,5-1.91,5-5,5-5-1.91-5-5Zm30,5c3.09,0,5-1.91,5-5s-1.91-5-5-5-5,1.91-5,5,1.91,5,5,5Zm-18,7c0-2.47-1.53-4-4-4h-6c-2.47,0-4,1.53-4,4v7h14v-7Zm-23,17h32v-6H16v6Zm44-21h-6c-2.11,0-3.53.71-4.8,2.4l-3.45,4.6h-2.75v4h4.75l4.25-5.67v15.67h12v-17c0-2.47-1.53-4-4-4Zm-53-3c3.09,0,5-1.91,5-5s-1.91-5-5-5-5,1.91-5,5,1.91,5,5,5Zm14,14v-4h-2.75l-3.45-4.6c-1.27-1.69-2.69-2.4-4.8-2.4h-6c-2.47,0-4,1.53-4,4v17h12v-15.67l4.25,5.67h4.75Z"
-                                                    fill="#37352F"
-                                                />
-                                            </svg>
-                                            <span>팀 구성</span>
-                                        </SummaryItemTitle>
-                                        <SummaryItemContent>
-                                            <TeamTag>개인프로젝트</TeamTag>
-                                        </SummaryItemContent>
-                                    </ModalSummary>
-                                    <SubDivider />
-                                    <ModalComment>
-                                        <ProfileImage src={profileImage} alt="profile" width={21} />
-                                        <CommentContent>
-                                            <span>mijin</span>
-                                            <p>커버 이미지는 Pixlr E를 사용해서 직접 제작했어요.</p>
-                                        </CommentContent>
-                                    </ModalComment>
-                                    <SubDivider />
-                                </ModalSummaryContainer>
-                            </ModalTextContainer>
+                            <ModalContentWrapper>
+                                <ModalImageContainer $todoLogo={todoLogo}>
+                                    <ModalImage src={todoTemplateImage} alt="todo template" />
+                                </ModalImageContainer>
+                                <ModalTextContent>
+                                    <ModalTitle>To Do List</ModalTitle>
+                                    <ModalSummaryContainer>
+                                        <ModalSummary>
+                                            <SummaryItemTitle>
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    width="21"
+                                                    height="21"
+                                                    viewBox="0 0 64 64"
+                                                >
+                                                    <defs fill="#37352F" />
+                                                    <path
+                                                        d="m32,8c-14.81,0-24,8.43-24,22,0,6.92,2.4,12.49,6.7,16.3l-6.7,6.7,2,3c5.42,0,10.43-1.8,14.45-4.84,2.32.55,4.84.84,7.55.84,14.81,0,24-8.43,24-22s-9.19-22-24-22Zm4,37h-8v-6h8v6Zm0-18l-2,8h-4l-2-8v-12h8v12Z"
+                                                        fill="#37352F"
+                                                    />
+                                                </svg>
+                                                <span>간단 소개</span>
+                                            </SummaryItemTitle>
+                                            <SummaryItemContent>
+                                                <p>해야 하는 일들의 정리를 도와주는 web application입니다.</p>
+                                            </SummaryItemContent>
+                                            <SummaryItemTitle>
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    width="21"
+                                                    height="21"
+                                                    viewBox="0 0 24 24"
+                                                    fill="none"
+                                                >
+                                                    <path
+                                                        d="M12 20L4.6797 10.8496C4.34718 10.434 4.18092 10.2262 4.13625 9.9757C4.09159 9.72524 4.17575 9.47276 4.34407 8.96778L5.0883 6.73509C5.52832 5.41505 5.74832 4.75503 6.2721 4.37752C6.79587 4 7.49159 4 8.88304 4H15.117C16.5084 4 17.2041 4 17.7279 4.37752C18.2517 4.75503 18.4717 5.41505 18.9117 6.73509L19.6559 8.96778C19.8243 9.47276 19.9084 9.72524 19.8637 9.9757C19.8191 10.2262 19.6528 10.434 19.3203 10.8496L12 20ZM12 20L15.5 9M12 20L8.5 9M19.5 10L15.5 9M15.5 9L14 5M15.5 9H8.5M10 5L8.5 9M8.5 9L4.5 10"
+                                                        stroke="#37352F"
+                                                        strokeLinecap="round"
+                                                    />
+                                                </svg>
+                                                <span>skills & tool</span>
+                                            </SummaryItemTitle>
+                                            <SummaryItemContent>
+                                                <ToDoSkills />
+                                            </SummaryItemContent>
+                                            <SummaryItemTitle>
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    width="21"
+                                                    height="21"
+                                                    viewBox="0 0 64 64"
+                                                >
+                                                    <defs fill="#37352F" />
+                                                    <path
+                                                        d="m48,12v-6h-6v6h-20v-6h-6v6h-8v40h48V12h-8Zm2,35H14v-19h36v19Z"
+                                                        fill="#37352F"
+                                                    />
+                                                </svg>
+                                                <span>진행 기간</span>
+                                            </SummaryItemTitle>
+                                            <SummaryItemContent>
+                                                <p>2023년 8월 18일 → 2023년 8월 29일</p>
+                                            </SummaryItemContent>
+                                            <SummaryItemTitle>
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    width="21"
+                                                    height="21"
+                                                    viewBox="0 0 64 64"
+                                                >
+                                                    <defs fill="#37352F" />
+                                                    <path
+                                                        d="m27,19c0-3.09,1.91-5,5-5s5,1.91,5,5-1.91,5-5,5-5-1.91-5-5Zm30,5c3.09,0,5-1.91,5-5s-1.91-5-5-5-5,1.91-5,5,1.91,5,5,5Zm-18,7c0-2.47-1.53-4-4-4h-6c-2.47,0-4,1.53-4,4v7h14v-7Zm-23,17h32v-6H16v6Zm44-21h-6c-2.11,0-3.53.71-4.8,2.4l-3.45,4.6h-2.75v4h4.75l4.25-5.67v15.67h12v-17c0-2.47-1.53-4-4-4Zm-53-3c3.09,0,5-1.91,5-5s-1.91-5-5-5-5,1.91-5,5,1.91,5,5,5Zm14,14v-4h-2.75l-3.45-4.6c-1.27-1.69-2.69-2.4-4.8-2.4h-6c-2.47,0-4,1.53-4,4v17h12v-15.67l4.25,5.67h4.75Z"
+                                                        fill="#37352F"
+                                                    />
+                                                </svg>
+                                                <span>팀 구성</span>
+                                            </SummaryItemTitle>
+                                            <SummaryItemContent>
+                                                <TeamTag>개인프로젝트</TeamTag>
+                                            </SummaryItemContent>
+                                        </ModalSummary>
+                                        <SubDivider />
+                                        <ModalComment>
+                                            <ProfileImage src={profileImage} alt="profile" width={21} />
+                                            <CommentContent>
+                                                <span>mijin</span>
+                                                <p>커버 이미지는 Pixlr E를 사용해서 직접 제작했어요.</p>
+                                            </CommentContent>
+                                        </ModalComment>
+                                        <SubDivider />
+                                    </ModalSummaryContainer>
+                                    <ModalContentSection>
+                                        <SectionTitle>🔗 Link</SectionTitle>
+                                        <LinkItem>
+                                            <span>⪧ GitHub(source)</span>
+                                            <LinkItemBox></LinkItemBox>
+                                        </LinkItem>
+                                    </ModalContentSection>
+                                </ModalTextContent>
+                            </ModalContentWrapper>
                         </ProjectDetailsModal>
                     </>
                 )}
